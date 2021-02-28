4440251adc503e1fdf2a4dd9a85e587d4606ff0e
Brian Alvis <francesc.alvis@globant.com>
2021-02-28 09:27:27 -0500

    actualizando bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,21 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Bon Jovi"));
+bands.addBand(new Band("Paramore"));
+bands.addBand(new Band("Metallica"));
+
+console.log(bands);
+
 //Mensajes de sockets
 io.on('connection', client => {
     console.log("Cliente conectado");
+
+    client.emit('active-bands',bands.getBands());
+
     client.on('disconnect', () => { 
         console.log("Cliente desconectado");
     });
@@ -10,4 +24,25 @@ io.on('connection', client => {
         console.log("Mensaje",payload);
         io.emit('mensaje',{admin:'Nuevo Mensajon!'});
     })
+
+    client.on('emitir-mensaje',(payload) => {
+        console.log("Mensaje de nuevo mensaje",payload);
+        client.broadcast.emit('nuevo-mensaje',payload)
+    })
+
+    client.on('vote-band',(payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands',bands.getBands());
+    });
+
+    client.on('add-band',(payload) => {
+        bands.addBand(new Band(payload["name"]));
+        io.emit('active-bands',bands.getBands());
+    });
+
+    client.on('delete-band',(payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands',bands.getBands());
+    });
+
   });
